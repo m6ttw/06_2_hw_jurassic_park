a50d7fd6dbdd59b2a06340bcf7ce99272fbb38e0
m6ttw <matthewrhyswilliams@live.com>
2021-01-06 20:54:20 +0000

    2 more tests passing

--- hw_tdd_jurassic_park_start/models/park.js
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -2,6 +2,14 @@ const Park = function(name, ticketPrice) {
     this.name = name;
     this.ticketPrice = ticketPrice;
     this.dinosaurCollection = [];
-}
+};
+
+Park.prototype.addDinosaur = function (dinosaur) {
+    this.dinosaurCollection.push(dinosaur);
+};
+
+Park.prototype.removeDinosaur = function (dinosaur) {
+    this.dinosaurCollection.pop(dinosaur);
+};
 
 module.exports = Park;
--- hw_tdd_jurassic_park_start/specs/park_spec.js
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -4,27 +4,46 @@ const Dinosaur = require("../models/dinosaur.js");
 
 describe("Park", function() {
 
+	let dinosaur1;
+	let dinosaur2;
+	let dinosaur3;
+	let dinosaur4;
 	let park;
 
 	beforeEach(function () {
+		dinosaur1 = new Dinosaur("T-rex", "Carnivore", 50)
+		dinosaur2 = new Dinosaur("Brachiosaurus", "Herbivore", 25)
+		dinosaur3 = new Dinosaur("Triceratops", "Herbivore", 30)
+		dinosaur4 = new Dinosaur("Velociraptor", "Carnivore", 40)
 		park = new Park("Jurassic Park", 550, []);
 	})
 
 	it("should have a name", function() {
-		const actual = park.name;
-		assert.strictEqual(actual, "Jurassic Park");
+		const actual = "Jurassic Park";
+		assert.strictEqual(actual, park.name);
 	});
 
 	it("should have a ticket price", function() {
-		const actual = park.ticketPrice;
-		assert.strictEqual(actual, 550);
+		const actual = 550;
+		assert.strictEqual(actual, park.ticketPrice);
 	});
 
-	it("should have a collection of dinosaurs");
+	it("should have a collection of dinosaurs", function() {
+		const actual = [];
+		assert.deepStrictEqual(actual, park.dinosaurCollection)
+	});
 
-	it("should be able to add a dinosaur to its collection");
+	it("should be able to add a dinosaur to its collection", function() {
+		park.addDinosaur(dinosaur1);
+		const expected = [dinosaur1];
+		assert.deepStrictEqual(park.dinosaurCollection, expected)
+	});
 
-	it("should be able to remove a dinosaur from its collection");
+	it("should be able to remove a dinosaur from its collection", function() {
+		park.removeDinosaur(dinosaur1);
+		const expected = [];
+		assert.deepStrictEqual(park.dinosaurCollection, expected)
+	});
 
 	it("should be able to find the dinosaur that attracts the most visitors");
 
